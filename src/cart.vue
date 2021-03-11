223c85dde189929f976f5f82df0a797893f270b9
oberial <245259423@qq.com>
2021-03-11 17:41:38 +0800

    feat: vue基础功能

---
+++ src/cart.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-for="item in items" :key="item.id">
+        {{ item }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: value
+            }
+        },
+        methods: {
+            name() {
+                
+            }
+        },
+        computed: {
+            name() {
+                return this.data 
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
